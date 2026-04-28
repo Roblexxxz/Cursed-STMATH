83c0390fc13812b5c068b77ae7fdc16aa3e4a855
Roblexxxz <daniela4354@mytusd.org>
2026-04-28 09:19:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert("Welcome to Cursed STMATH this is a cursed version of cursed stmath that is BETTER THAN REAL STMATH >:(");
+alert("Welcome to Cursed STMATH this is a cursed version of cursed stmath that is BETTER THAN REAL STMATH");
